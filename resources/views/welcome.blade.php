2c77d86a11d11ba6c05929ab1548cb83139ff02a
Wesley Barbosa <34480299+wbs2A@users.noreply.github.com>
2019-05-06 18:06:58 -0400

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,6 +83,7 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
+
                 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                 <script>
                     (adsbygoogle = window.adsbygoogle || []).push({
