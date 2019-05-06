65e8aa0016578de15d5eea91330208882e52cfd8
wbs2a <wbs0616@gmail.com>
2019-05-06 18:20:35 -0400

    testing automatic deploy1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    O site dahora
+                    A Valzinha é dahora
                 </div>
                 Aqui vão meus testes
 
